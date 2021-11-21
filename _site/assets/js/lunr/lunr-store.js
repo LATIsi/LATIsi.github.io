41164ad16db9efe9053ebf4fcea88547781c7c75
LATIsi <p.kmj9911@gmail.com>
2021-11-21 17:48:02 +0900

    1.2.0 카테고리 오류 다시 초기화

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,25 +1,25 @@
 var store = [{
         "title": "jekyll 블로그 제작관련 기록",
-        "excerpt":"추후 추가되는 글입니다. 로컬서버 구동 할 때! cmd에서 bundle exec jekyll serve 으로 로컬 서버에서 구동확인 구동했을시 관련 오류. -- cannot load such file -- webrick 오류가 나서 bundle add webrick을 설치한뒤 다시 bundle exec jekyll serve 실행. 참고 글 : https://junho85.pe.kr/1850 블로그 글 작성시 작성 방법! title: # 제목...","categories": ["post","blog"],
+        "excerpt":"추후 추가되는 글입니다. 로컬서버 구동 할 때! cmd에서 bundle exec jekyll serve 으로 로컬 서버에서 구동확인 구동했을시 관련 오류. -- cannot load such file -- webrick 오류가 나서 bundle add webrick을 설치한뒤 다시 bundle exec jekyll serve 실행. 참고 글 : https://junho85.pe.kr/1850 블로그 글 작성시 작성 방법! title: # 제목...","categories": ["blog"],
         "tags": [],
-        "url": "/post/blog/blog_command/",
+        "url": "/blog/blog_command/",
         "teaser": null
       },{
         "title": "git 명령어 정리",
-        "excerpt":"추후 추가되는 글입니다. 내가 만들던 프로젝트나 공부용 파일들을 백업하는김에 이것저것 뒤져보다 최근에서야 실무에서도 많이쓰이는 git을 알고 사용하기로 결정! 그런데… 팀 프로젝트를 했을적에는 git의 정체도 몰랐었다!(!!!!!) 서로 카톡으로 프로젝트의 파일을 수정하며 결합했다가 그럼에도 오류났을때 비명을 질렀던 기억이 난다.. 지금이라도 익숙해지자 싶어 공부를 시작하고야 말았는데! 나 자신을 위한 git 명령어를 노션에 정리해두었지만...","categories": ["post","Git"],
+        "excerpt":"추후 추가되는 글입니다. 내가 만들던 프로젝트나 공부용 파일들을 백업하는김에 이것저것 뒤져보다 최근에서야 실무에서도 많이쓰이는 git을 알고 사용하기로 결정! 그런데… 팀 프로젝트를 했을적에는 git의 정체도 몰랐었다!(!!!!!) 서로 카톡으로 프로젝트의 파일을 수정하며 결합했다가 그럼에도 오류났을때 비명을 질렀던 기억이 난다.. 지금이라도 익숙해지자 싶어 공부를 시작하고야 말았는데! 나 자신을 위한 git 명령어를 노션에 정리해두었지만...","categories": ["Git"],
         "tags": [],
-        "url": "/post/git/git-command/",
+        "url": "/git/git-command/",
         "teaser": null
       },{
         "title": "JS 객체 관련 with , in에 대하여",
-        "excerpt":"JS 공부를 하다가  with과 in 관련해서 정리를 해두려고 한다.  ","categories": ["post","JavaScript"],
+        "excerpt":"JS 공부를 하다가  with과 in 관련해서 정리를 해두려고 한다.  ","categories": ["JavaScript"],
         "tags": [],
-        "url": "/post/javascript/JS-object-with,in/",
+        "url": "/javascript/JS-object-with,in/",
         "teaser": null
       },{
         "title": "개발 목표 관련",
-        "excerpt":"올해 대학교를 졸업하고나서 10월 중 후반부터 본격적인 프런트공부를 하기 시작했다! 왜냐면 나는 그림그리는 것을 굉장히 좋아한다. 오죽하면 중고딩때 매일 그림공책을 들고 건물을 따라 그리거나 크로키 하는 것도 좋아했고. 그때문인가 고등학교도 디자인학과에 들어가서 그때 웹디자인도 처음으로 해봤다. 근데 디자인만으로 만족 못하고 그 안의 기능도 내 스스로 개발하고 싶은걸 어떻게 하라는건지 ;(...","categories": ["post","일상"],
+        "excerpt":"올해 대학교를 졸업하고나서 10월 중 후반부터 본격적인 프런트공부를 하기 시작했다! 왜냐면 나는 그림그리는 것을 굉장히 좋아한다. 오죽하면 중고딩때 매일 그림공책을 들고 건물을 따라 그리거나 크로키 하는 것도 좋아했고. 그때문인가 고등학교도 디자인학과에 들어가서 그때 웹디자인도 처음으로 해봤다. 근데 디자인만으로 만족 못하고 그 안의 기능도 내 스스로 개발하고 싶은걸 어떻게 하라는건지 ;(...","categories": ["일상"],
         "tags": [],
-        "url": "/post/%EC%9D%BC%EC%83%81/toyproject/",
+        "url": "/%EC%9D%BC%EC%83%81/toyproject/",
         "teaser": null
       }]
